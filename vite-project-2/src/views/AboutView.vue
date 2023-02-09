47ceb493f5c999ec831beb74e24d87991dcbb40b
rainliao742@gmail.com <rainliao742@gmail.com>
2023-02-09 15:29:01 +0800

    vite env----erect

---
+++ vite-project-2/src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  {{ data }}
+</template>
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+    const apiUrl = import.meta.env.VITE_PATH;
+    //console.log(import.meta.env.VITE_PATH);
+    this.$http.get(apiUrl).then((res) => {
+      console.log(res.data);
+      this.data = res.data.results[0];
+      console.log(this.data);
+    });
+  },
+};
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ vite-project/src/components/CardComponent.vue
@@ -0,0 +1,17 @@
+<template>
+	<h2>
+	卡片
+	</h2>
+	<br>
+	<p>{{ text }}</p>
+</template>
+
+<script>
+export default{
+	data(){
+		return{
+			text:'我是一段文字',
+		}
+	}
+}
+</script>
